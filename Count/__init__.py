21772d99c2d3852aec5cc6cd3e446f0f03ebfcef
Cota <Cotalorr@outlook.com>
2024-01-01 18:34:38 +0800

    COUNTABLE VERSION

--- Count/__init__.py
+++ Count/__init__.py
@@ -33,15 +33,15 @@ for i in range(len(content_list)):
         break
 new_char = [ch(content_list[i].lower())for i in range(p + 1, len(content_list))]
 new_char2 = [item for item in new_char if item not in ori_char]
+ci = 0
 
 with open("C:/Users/fctjg/Desktop/Abstracted Document.txt", 'w') as file2:
     file2.write("The new word list:" + '\n')
     for item in new_char2:
+        ci += 1
         file2.write(item + '\n')
-
+    file2.write(f'Totally of {ci} words.')
 # cut = "~!@#$%^&*()_{}|:<>?，。/、】【’；·1234567890-=+——：“”《》？`"
-# ci = 0
 # for a in paragraph:
 #     if a not in cut:
-#         ci += 1
 # print(ci)
